ad835ca4b4a2c6992029d5f36e7ad4c36c1373d6
codebaard <github@juliusneudecker.com>
2020-08-12 18:58:56 +0200

    Database preliminaries

---
+++ cms.ar.xarchitecture.de/cms.ar.xarchitecture.de/Models/Asset.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cms.ar.xarchitecture.de.Models
+{
+    public class Asset
+    {
+
+    }
+}
---
+++ cms.ar.xarchitecture.de/cms.ar.xarchitecture.de/Models/Creator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cms.ar.xarchitecture.de.Models
+{
+    public class Creator
+    {
+    }
+}
