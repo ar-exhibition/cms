ea82e921cd58f98e3f5e8cdcb6beb61e4c47d707
codebaard <github@juliusneudecker.com>
2020-09-28 22:34:31 +0200

    API WIP

---
+++ cms.ar.xarchitecture.de/cms.ar.xarchitecture.de/Models/Content.cs
@@ -0,0 +1,16 @@
+﻿using cms.ar.xarchitecture.de.cmsDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cms.ar.xarchitecture.de.Models
+{
+    public class Content
+    {
+        public IEnumerable<SceneAsset> assets;
+        public IEnumerable<Anchor> anchors;
+        public IEnumerable<Scene> scenes;
+    }
+
+}
