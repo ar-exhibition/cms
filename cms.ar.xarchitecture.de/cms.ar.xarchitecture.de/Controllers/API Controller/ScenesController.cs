56d993ed90539bcc073e7572ccda8db2109c5233
codebaard <github@juliusneudecker.com>
2020-10-05 21:08:18 +0200

    Scene API WIP

---
+++ cms.ar.xarchitecture.de/cms.ar.xarchitecture.de/Controllers/API Controller/ScenesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using cms.ar.xarchitecture.de.Models.Wrapper;
+using cms.ar.xarchitecture.de.cmsXARCH;
+
+namespace cms.ar.xarchitecture.de.Controllers.API_Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScenesController : ControllerBase
+    {
+        // GET: api/<ScenesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ScenesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ScenesController>
+        [HttpPost]
+        public void Post(SceneSubmissionValues values)
+        {
+            Scene newRecord = new Scene();
+
+            newRecord.SceneName = values.name;
+            //continue here...
+
+        }
+
+        // PUT api/<ScenesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ScenesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ cms.ar.xarchitecture.de/cms.ar.xarchitecture.de/Models/Wrapper/SceneSubmissionValues.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace cms.ar.xarchitecture.de.Models.Wrapper
+{
+    public class SceneSubmissionValues
+    {
+        public string name {get; set;}
+        public IFormFile sceneFile { get; set; } 
+    }
+}
