584b5c3eb4f6a39fb47386e5c29a1faa2166ad05
Julius Neudecker <Github@juliusneudecker.com>
2020-10-28 21:06:20 +0100

    added files - error on frontend

---
+++ cms.ar.xarchitecture.de/cms.ar.xarchitecture.de/Controllers/Frontend/AssetShowcase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+
+namespace cms.ar.xarchitecture.de.Controllers.Frontend
+{
+    public class AssetShowcaseController : Controller
+    {
+        IWebHostEnvironment _hostingEnvironment;
+
+        public AssetShowcaseController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET: AssetShowcaseController
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
